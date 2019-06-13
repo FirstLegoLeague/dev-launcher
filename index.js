6d01944ef1fc8524ef2bee2bea8769fc777385d3
Roy <2903801+2roy999@users.noreply.github.com>
2019-06-13 09:33:28 +0300

    change order of the options

--- index.js
+++ index.js
@@ -11,22 +11,22 @@ const endArgs = doubleDashIndex === -1 ? [] : process.argv.slice(doubleDashIndex
 
 caporal
   .version(projectVersion)
-  .option('--port, -p <port>', 'In which port to open the primary module. Default to: 3000', caporal.INT)
-  .option('--log-level, -L <logLevel>', 'In which port to open the primary module',
-    ['fatal', 'error', 'warn', 'info', 'debug'])
-  .option('--data-directory, -d <dataDirectory>', 'Directory to store service data. Default to: ./data')
-  .option('--secondary-modules, -s <secondaryModule>',
-    'Module\'s url to pass to the primary module in the format "#module_name#=#origin#', caporal.REPEATABLE)
   .option('--config-file <configFile>', 'The config file for the primary module. Default to: ./module-config.json')
-  .option('--mongo-uri <mongoUri>',
-    'The mongo uri for the primary module. Default to: mongodb://localhost:27017/##module-name##')
-  .option('--secret <secret>', 'The secret token. Default: secret', /[A-Za-z01-9]/g)
-  .option('--mhub-pass <mhubPass>', 'The mhub protected password', /[A-Za-z01-9]/g)
-  .option('--no-color', 'Disable colors', caporal.BOOL)
+  .option('--data-directory, -d <dataDirectory>', 'Directory to store service data. Default to: ./data')
   .option('--inspect [host:port]', 'Adding node --inspect options to the node process. ' +
     'Works only when module command omitted')
   .option('--inspect-brk [host:port]', 'Adding node --inspect-brk options to the node process. ' +
     'Works only when module command omitted')
+  .option('--log-level, -L <logLevel>', 'In which port to open the primary module',
+    ['fatal', 'error', 'warn', 'info', 'debug'])
+  .option('--mhub-pass <mhubPass>', 'The mhub protected password', /[A-Za-z01-9]/g)
+  .option('--mongo-uri <mongoUri>',
+    'The mongo uri for the primary module. Default to: mongodb://localhost:27017/##module-name##')
+  .option('--no-color', 'Disable colors', caporal.BOOL)
+  .option('--port, -p <port>', 'In which port to open the primary module. Default to: 3000', caporal.INT)
+  .option('--secondary-modules, -s <secondaryModule>',
+    'Module\'s url to pass to the primary module in the format "#module_name#=#origin#', caporal.REPEATABLE)
+  .option('--secret <secret>', 'The secret token. Default: secret', /[A-Za-z01-9]/g)
   .argument('[moduleCommand]', 'The command to start the primary module')
   .argument('[-- moduleArguments...]', 'The arguments to pass the primary module\'s command')
   .action((args, options) => {
